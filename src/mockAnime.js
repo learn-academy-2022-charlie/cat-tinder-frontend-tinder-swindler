3526544a8ad268ae5d211b5d0031df9c098497a6
ios10 <trikshotzz05@gmail.com>
2022-06-30 15:55:27 -0700

    took away some

--- src/mockAnime.js
+++ src/mockAnime.js
@@ -33,41 +33,6 @@ let anime = [
     age: 26,
     enjoys: "making unique poison that can infect demons",
     image: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/04/shinobu-demon-slayer-sword.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5"
-},
-  {
-    id: 6,
-    name: "Bakugou Katsuki",
-    age: 18,
-    enjoys: "Blowing things up",
-    image: "https://static.wikia.nocookie.net/bokunoheroacademia/images/1/17/Katsuki_Bakugo_Winter_Costume_%28Anime%29.png/revision/latest?cb=20201219091112"
-  },
-  {
-    id: 7,
-    name: "Baki Hanma",
-    age: 21,
-    enjoys: "Fighting some of the best martial artist in world",
-    image: "https://i0.wp.com/www.animegeek.com/wp-content/uploads/2020/09/Baki-Season-4-release-date-Netflixs-Baki-Son-of-Ogre-sequel-Baki-Hanma-manga.jpg?resize=1024%2C576&ssl=1"
-  },
-  {
-    id: 8,
-    name: "Revy",
-    age: 28,
-    enjoys: "Likes shooting guns",
-    image: "https://imgix.ranker.com/user_node_img/50087/1001738585/original/whether-we-live-or-die-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=650"
-  },
-  {
-    id: 9,
-    name: "Obito",
-    age: 25,
-    enjoys: "Likes taking over the world",
-    image: "https://otakukart.com/wp-content/uploads/2021/11/140034.jpg"
-  },
-  {
-    id: 10,
-    name: "Esdeath",
-    age: 33,
-    enjoys: "Likes to freeze things she doesn't like",
-    image: "http://pm1.narvii.com/6861/2582827e135640017caafdf1940b85cc4535c299r1-1920-1080v2_00.jpg"
-  }
+}
   ]
   export default anime
